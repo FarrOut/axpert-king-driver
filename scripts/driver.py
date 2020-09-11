244616715c8674442de529fdf57d330fbfc14494
Greg Farr <farr.gs@gmail.com>
2020-09-11 14:34:34 +0200

    parse to JSON

--- scripts/driver.py
+++ scripts/driver.py
@@ -29,7 +29,35 @@ def get_general_status():
     _regex = r"\b(\d{1,3}\.?\d{0,2})\b"
     _match = re.search(_regex, str(_res))
     _matches = [x.group() for x in re.finditer( _regex, str(_res))]
-    print('Matches->',_matches)
+
+    _grid_volt                      = _matches[0]
+    _grid_freq                      = _matches[1]
+    _ac_output_volt                 = _matches[2]
+    _ac_output_freq                 = _matches[3]
+    _ac_output_apparent_power       = _matches[4]
+    _ac_output_active_power         = _matches[5]
+    _output_load_perc               = _matches[6]
+    _bus_volt                       = _matches[7]
+    _battery_volt                   = _matches[8]
+    _battery_charging_current       = _matches[9]
+    _battery_capacity               = _matches[10]
+    _inverter_heat_sink_temp        = _matches[11]
+    # _pv_input_current_for_battery   = _matches[12]
+    # _pv_input_volt                  = _matches[13]
+    # _battery_volt_from_scc          = _matches[14]
+    # _battery_discharge_current      = _matches[15]
+    # _device_status                  = _matches[16]
+
+    _dataset = {
+        "query_cmd": _cmd,
+        "data": {
+            "grid_voltage": _grid_volt
+        }
+    }
+
+    _json = json.dumps(_dataset)
+
+    print(_json)
 
 # ------------------ Generic Functions ----------------------
 
